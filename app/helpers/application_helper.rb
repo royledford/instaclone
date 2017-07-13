6a273cb38c934d93258cf062e09bbcff82b4a094
roy ledford <bluesixty@gmail.com>
2017-07-13 19:12:41 -0400

    Add a helper for the page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(page_title)
+    if page_title.present?
+      content_for :title, page_title.to_s
+    else
+      content_for :title, "InstaClone"
+    end
+  end
+
 end
