e29381d56aae3374be1faa7b365e9b248733520b
roy ledford <bluesixty@gmail.com>
2017-07-13 11:00:13 -0400

    Add user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713133327) do
+ActiveRecord::Schema.define(version: 20170713145439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name", null: false
+    t.string "username", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "email"
