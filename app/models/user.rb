2e25de9276e1d7971e758b34f522134faa2fc511
roy ledford <bluesixty@gmail.com>
2017-07-14 22:42:03 -0400

    Add unique index to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_many :posts
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
+  before_save { self.email = email.downcase }
+
+
+  validates :first_name, presence: true, length: { maximum: 50}
+  validates :last_name, presence: true, length: { maximum: 50}
+  validates :username, presence: true, length: { maximum: 30},
+            uniqueness: {case_sensitive: false}
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 254},
+            format: {with: EMAIL_REGEX}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,84 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(username: "lustforlife", first_name: "Iggy", last_name: "Pop", email: "iggy@pop.com")
+  end
+
+  test "User should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "       "
+    assert_not @user.valid?
+  end
+
+  test "first_name should be present" do
+    @user.first_name = "       "
+    assert_not @user.valid?
+  end
+
+  test "last_name should be present" do
+    @user.last_name = "       "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.first_name = "       "
+    assert_not @user.valid?
+  end
+
+  test "Username > 30 characters" do
+    @user.username = "a" * 31
+    assert_not @user.valid?
+  end
+
+  test "first_name > 50 characters" do
+    @user.first_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "last_name > 50 characters" do
+    @user.last_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email > 254 characters" do
+    @user.email = "a" * 255
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.c]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "INvalid email address are not allowed" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} is INvalid"
+    end
+  end
+
+  test "Username should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "Emails should be all lowercase" do
+    test_email = "Iggy@pop.Com"
+    @user.email = test_email
+    @user.save
+    assert_equal test_email.downcase, @user.reload.email
+  end
+
 end
