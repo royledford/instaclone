4f78a962679dcc93a65734176049b01170021b00
roy ledford <bluesixty@gmail.com>
2017-07-15 20:52:39 -0400

    Update flash messages on login page to display correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log them in and send to profile page (show)
+    else
+      # failed login message
+      flash.now[:warn] = "Ruh Roh! The email and password you entered don't match our records. Please try again."
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,14 @@ Rails.application.routes.draw do
   get '/about',  to: 'static_pages#about'
 
   # Users
-  get '/signup', to: 'users#new'
+  get '/signup',  to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
 
   # handle login sessions
-  get '/login',   to: 'sessions#new'
-  post '/login',   to: 'sessions#create'
-  delete '/login',   to: 'sessions#destroy'
+  get '/login',       to: 'sessions#new'
+  post '/login',      to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
 
 
   resources :posts
