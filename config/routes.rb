f56fdb307aaa9e85799e4fb22286a803495b7d91
roy ledford <bluesixty@gmail.com>
2017-07-15 20:03:28 -0400

    make and Style the login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   # get 'static_pages/home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
+  get '/help',   to: 'static_pages#help'
+  get '/about',  to: 'static_pages#about'
 
+  # Users
+  get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  resources :users
+
+  # handle login sessions
+  get 'login',   to: 'sessions#new'
+  get 'login',   to: 'sessions#create'
+  get 'login',   to: 'sessions#destroy'
+
 
   resources :posts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
