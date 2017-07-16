00b4342080b76486380ac66e231610b31ac7cb36
roy ledford <bluesixty@gmail.com>
2017-07-16 17:45:40 -0400

    Add remember me functionality to models and controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log them in and send to profile page (show)
       log_in user
+      remember user
       redirect_to user
     else
       # failed login message
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,39 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = { value: user.id,
+                                           expires: 6.months.from_now.utc}
+    cookies.permanent[:remember_token] = { value: user.remember_token,
+                                            expires: 6.months.from_now.utc}
+
+    # To make them permanent, replace above with this
+    # cookies.permanent.signed[:user_id] = user.id
+    # cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # delete the db remember and cookies
+  def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+  end
+
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      # User is authenticated so just return them
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      # User is not logged in, get from db
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies{:remember_token})
+        # User found in db and has a remember set = true
+        login user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -13,6 +44,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
